df56add213c8e1980cd0fa6b6cf3b83dab73ae98
Siddharth Agarwal <sid0@fb.com>
2014-12-13 12:25:42 -0800

    add license and other classifiers and bump version

--- setup.py
+++ setup.py
@@ -4,9 +4,14 @@ from distutils.core import setup, Extension
 
 setup(
     name="fb-re2",
-    version="0.1.1",
+    version="0.1.2",
     url="https://github.com/facebook/pyre2",
     description="Python wrapper for Google's RE2",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+    ],
     author="David Reiss",
     author_email="dreiss@fb.com",
     maintainer="Siddharth Agarwal",
