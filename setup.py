176a26a1ee1a1f962026d311d086376d8d4c74d9
Siddharth Agarwal <sid0@fb.com>
2014-12-13 11:37:48 -0800

    prepare for release on PyPI

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@
 from distutils.core import setup, Extension
 
 setup(
-    name="re2",
+    name="fb-re2",
     version="0.1.0",
+    url="https://github.com/facebook/pyre2",
     description="Python wrapper for Google's RE2",
     author="David Reiss",
+    author_email="dreiss@fb.com",
+    maintainer="Siddharth Agarwal",
+    maintainer_email="sid0@fb.com",
     py_modules = ["re2"],
     ext_modules = [Extension("_re2",
       sources = ["_re2.cc"],
