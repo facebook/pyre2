9ed6449fd3aeaa698260e91b0f4c4cc6a0013d0e
Siddharth Agarwal <sid0@fb.com>
2016-10-09 02:59:06 -0700

    prepare for release 1.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup, Extension
 
 setup(
     name="fb-re2",
-    version="1.0.4",
+    version="1.0.5",
     url="https://github.com/facebook/pyre2",
     description="Python wrapper for Google's RE2",
     classifiers=[
