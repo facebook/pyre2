bf46a0bdc87c8e1af31e35eda93ca479fa92ee8b
Anton Tiurin <noxiouz@yandex.ru>
2018-11-29 21:43:51 +0000

    [tests] automate test environment
    Migrate from deprecated distutils to setuptools.
    To run tests: python setup.py test
    
    Add tox file to run tests against various Python versions
    
    Signed-off-by: Anton Tiurin <noxiouz@fb.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 setup(
     name="fb-re2",
@@ -17,6 +17,7 @@ setup(
     maintainer="Siddharth Agarwal",
     maintainer_email="sid0@fb.com",
     py_modules = ["re2"],
+    test_suite = "tests.test_match.TestMatch",
     ext_modules = [Extension("_re2",
       sources = ["_re2.cc"],
       libraries = ["re2"],
